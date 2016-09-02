c112354abc18e5bc6aa64e207fbd2353c987a96c
Oleksii Turaiev <oleksii.turaiev@globallogic.com>
2016-09-02 14:48:21 +0300

    Initial commit

---
+++ build.ps1
@@ -0,0 +1,21 @@
+﻿cls 
+
+# [p]sake is the same as psake but $Error is not polluted
+remove-module [p]sake
+
+# find psake's path
+$psakeModule = (Get-ChildItem (".\packages\psake*\tools\psake.psm1")).FullName | Sort-Object $_ | select -last 1
+
+Import-Module $psakeModule
+
+Invoke-psake -buildFile .\default.ps1 `
+			 -taskList Test `
+			 -properties @{ 
+				 "buildConfiguration" = "Release" ` 
+			     "buildPlatform" = "Any CPU" } `
+			-parameters @{ 
+				 "solutionFile" = ".\Build.sln" }
+
+Write-Host "Build exit code:" $LastExitCode
+
+exit $LastExitCode
